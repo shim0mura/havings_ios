b49d398827f33abf7d7c74d4da75b66c82921248
shim0mura <tatshimomura@gmail.com>
2016-05-21 11:44:28 +0900

    Initial Commit

---
+++ havings/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  havings
+//
+//  Created by Tatsuhiko Shimomura on 2016/05/21.
+//  Copyright © 2016年 Tatsuhiko Shimomura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
